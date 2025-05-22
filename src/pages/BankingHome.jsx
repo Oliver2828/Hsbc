e982abd55b5bb5b7de31b05de4d68c5618d410f0
kingsLib5 <libertykings88@gmail.com>
2025-05-22 13:53:08 +0100

    this is the commit to the banking1

--- src/pages/BankingHome.jsx
+++ src/pages/BankingHome.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Banking1 from '../components/Global-bank-components/Banking1';
 import Banking2 from '../components/Global-bank-components/Banking2';
 import Banking3 from '../components/Global-bank-components/Banking3';
 import Banking4 from '../components/Global-bank-components/Banking4';
@@ -6,6 +7,7 @@ import Banking4 from '../components/Global-bank-components/Banking4';
 function BankingHome() {
   return (
     <div>
+      <Banking1 />
       <Banking2 />
       <Banking3 />
       <Banking4 />

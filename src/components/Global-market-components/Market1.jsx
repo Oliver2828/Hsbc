4bed38fe3838876dabb3ef9a5d24aa133adcd891
Oliver2828 <oliverezenwaka95@gmail.com>
2025-05-23 15:28:50 +0100

    market

--- src/components/Global-market-components/Market1.jsx
+++ src/components/Global-market-components/Market1.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Market1() {
   return (
-    <div>Market1</div>
+    <>
+    
+      <div className=' bg-[url(././assets/global.jpg)] bg-no-repeat bg-cover bg-center grid h-[80vh]'>
+        <div className=' flex justify-center text-[60px] text-[white] font-light font-serif  items-center'>
+          <h2>Global Markets</h2>
+        </div>
+      </div>
+    
+    </>
   )
 }
 

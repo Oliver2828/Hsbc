811c5b3b316d542577bae1eab14252c6d7adde83
kingsLib5 <libertykings88@gmail.com>
2025-05-23 13:40:23 +0100

    created the necessary file fo the global market this is the commit for it

---
+++ src/components/Global-market-components/Market1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Market1() {
+  return (
+    <div>Market1</div>
+  )
+}
+
+export default Market1
---
+++ src/components/Global-market-components/Market2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Market2() {
+  return (
+    <div>Market2</div>
+  )
+}
+
+export default Market2
